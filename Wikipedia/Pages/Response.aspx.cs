5f353b3ca27c5230bf5019777113e6bf323af94c
Gabriel <gabrielbudau@gmail.com>
2014-05-26 23:28:00 -0700

    Am implementat pagina de 'Response (cc: Budau Gabriel, Petrica Bobe).'

--- Wikipedia/Pages/Response.aspx.cs
+++ Wikipedia/Pages/Response.aspx.cs
@@ -97,60 +97,38 @@ namespace Wikipedia.Pages
 
                 var user = manager.FindById(User.Identity.GetUserId());
                 
-                /*IdentityResult result = manager.AddPassword(user.Id, password.Text);*/
-                /*
+               // IdentityResult result = manager.AddPassword(user.Id, password.Text);
+                
                 string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
-                String queryStringPage = "INSERT INTO [Pages] ([Title], [Tags], [CreatedBy], [CreatedOn], [ModifiedBy], [ModifiedOn], [Bounty]) VALUES (@Title, @Tags, @CreatedBy, @CreatedOn, @ModifiedBy, @ModifiedOn, @Bounty); SELECT @Id=SCOPE_IDENTITY();";
-                String queryStringPageContent = "INSERT INTO [PagesContent] ([EditedBy], [EditedOn], [VersionNumber], [Content], [PageId], [Approved]) VALUES (@EditedBy, @EditedOn, @VersionNumber, @Content, @PageId, @Approved);";
-                //
+                String queryStringPage = "INSERT INTO [Projects_Solutions] ([User_id], [Project_id], [content]) VALUES (@u_id, @p_id, @content);";
 
-                string money = Bounty.Text;
                 SqlConnection myConnection = new SqlConnection(connectionString);
                 try
                 {
                     myConnection.Open();
 
                     SqlCommand myCommandPage = new SqlCommand(queryStringPage, myConnection);
-                    myCommandPage.Parameters.Add(new SqlParameter("Title", PageTitle.Text));
-                    myCommandPage.Parameters.Add(new SqlParameter("Tags", Tags.Text));
-                    myCommandPage.Parameters.Add(new SqlParameter("Bounty", Int32.Parse(Bounty.Text)));
-                    myCommandPage.Parameters.Add(new SqlParameter("CreatedBy", user.UserName));
-                    myCommandPage.Parameters.Add(new SqlParameter("CreatedOn", DateTime.Now));
-                    myCommandPage.Parameters.Add(new SqlParameter("ModifiedBy", user.UserName));
-                    myCommandPage.Parameters.Add(new SqlParameter("ModifiedOn", DateTime.Now));
-
-                    SqlParameter idParam = new SqlParameter("@Id", SqlDbType.Int, 4);
-                    idParam.Direction = ParameterDirection.Output;
-                    myCommandPage.Parameters.Add(idParam);
-
+                    myCommandPage.Parameters.Add(new SqlParameter("p_id", Request.QueryString["id"]));
+                    myCommandPage.Parameters.Add(new SqlParameter("content", PageContent.Value));
+                    myCommandPage.Parameters.Add(new SqlParameter("u_id", user.Id));
+                   
                     myCommandPage.ExecuteNonQuery();
 
-                    int myNewId = Convert.ToInt32(idParam.Value);
 
-                    SqlCommand myCommandPageContent = new SqlCommand(queryStringPageContent, myConnection);
-                    myCommandPageContent.Parameters.Add(new SqlParameter("EditedBy", user.UserName));
-                    myCommandPageContent.Parameters.Add(new SqlParameter("EditedOn", DateTime.Now));
-                    myCommandPageContent.Parameters.Add(new SqlParameter("Content", PageContent.Value));
-                    myCommandPageContent.Parameters.Add(new SqlParameter("PageId", myNewId));
-                    myCommandPageContent.Parameters.Add(new SqlParameter("VersionNumber", 1));
-                    myCommandPageContent.Parameters.Add(new SqlParameter("Approved", 1));
-
-                    myCommandPageContent.ExecuteNonQuery();
-                    
                     //Response.Redirect("~/Pages/AllPages.aspx?m=SetPwdSuccess&id=1");
-                    Response.Redirect("~/Pages/View.aspx?id="+ myNewId);
+                    Response.Redirect("~/Pages/View.aspx?id=" + Request.QueryString["id"]);
                 }
                 catch (Exception ex)
                 {
-                    ErrorMessage = ex.Message;
+                    ErrorMessage2 = ex.Message;
 
-                    errorMessage.Visible = !String.IsNullOrEmpty(ErrorMessage);
+                    errorMessage.Visible = !String.IsNullOrEmpty(ErrorMessage2);
                 }
                 finally
                 {
                     myConnection.Close();
                 }
-                 * */
+                
             }
         }
 
